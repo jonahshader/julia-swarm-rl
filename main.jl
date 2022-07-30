88e9f4dc6cd28ef86c666f5719202ed11dba8935
jonahshader <jonah.shader@gmail.com>
2022-07-30 00:43:19 -0400

    creature computation mostly implemented, really good performance so far

--- main.jl
+++ main.jl
@@ -16,3 +16,25 @@
 
 
 include("systems/creature.jl")
+include("raylib/raylib.jl")
+
+const s_w = 1920
+const s_h = 1080
+InitWindow(s_w, s_h, "Tree Growing Simulator")
+SetTargetFPS(165)
+
+# cool_tree = make_tree(7, 150.0, 0.75, 0.0, pi/8, 0.75)
+
+const creatures = make_creatures(8192, Float32(s_w), Float32(s_h), [100, 100], 512, 8, device=:gpu, rand_memory=true)
+while !WindowShouldClose()
+    BeginDrawing()
+        ClearBackground(Color(0, 0, 0, 255))
+        update_creatures!(creatures)
+        update_creatures!(creatures)
+        update_creatures!(creatures)
+        render_creatures(creatures)
+
+    EndDrawing()
+end
+
+CloseWindow()
--- systems/creature.jl
+++ systems/creature.jl
@@ -1,6 +1,7 @@
 include("nn.jl")
 
-using Raylib
+
+
 using CUDA
 
 mutable struct Creature
@@ -9,16 +10,24 @@ mutable struct Creature
     nn::NeuralNet
     scalars_temp::AbstractArray
     memory::AbstractArray
+    vision::AbstractArray
+    input::AbstractArray
 end
 
-function make_creatures(n::Int, width::Float32, height::Float32; hidden_layer_sizes::Vector{Int}, memory_size::Int, vision_size::Int; device::Symbol = :cpu, activation::Function = tanh)
+function make_creatures(n::Int, width::Float32, height::Float32, hidden_layer_sizes::Vector{Int}, memory_size::Int, vision_size::Int; device::Symbol = :cpu, activation::Function = tanh, rand_memory::Bool = false)
     # 4 channels per ray, all of memory is read in
     input_size = vision_size * 4 + memory_size
     # x y mag, all of memory has value and proportion
     output_size = 3 + memory_size * 2
     position = vcat(rand(Float32, 1, n) * width, rand(Float32, 1, n) * height)
     velocity = zeros(Float32, 2, n)
-    memory = randn(Float32, 32, n)
+    if rand_memory
+        memory = randn(Float32, memory_size, n)
+    else
+        memory = zeros(Float32, memory_size, n)
+    end
+    vision = zeros(Float32, vision_size * 4, n)
+    input = zeros(Float32, input_size, n)
 
     nn = make_net([input_size, hidden_layer_sizes..., output_size], activation, device=device)
     change_batch_size!(nn, n)
@@ -26,24 +35,33 @@ function make_creatures(n::Int, width::Float32, height::Float32; hidden_layer_si
     scalars_temp = zeros(Float32, n)
 
     if device != :cpu
-        position = CuMatrix(Float32, position)
-        velocity = CuMatrix(Float32, velocity)
-        memory = CuMatrix(Float32, memory)
-        scalars_temp = CuMatrix(Float32, scalars_temp)
+        position = CuMatrix(position)
+        velocity = CuMatrix(velocity)
+        memory = CuMatrix(memory)
+        vision = CuMatrix(vision)
+        input = CuMatrix(input)
+        scalars_temp = CuVector(scalars_temp)
     end
-    Creature(position, velocity, nn, scalars_temp, memory)
+    Creature(position, velocity, nn, scalars_temp, memory, vision, input)
 end
 
 
 # TODO: where i left off: rewrite this to use view on memory, and i guess make placeholder views for vision
 function update_creatures!(creatures::Creature)
-    nn_out = forward!(creatures.nn, creatures.memory)
+    nn_input_vision = view(creatures.input, 1:size(creatures.vision)[1], :)
+    nn_input_memory = view(creatures.input, size(creatures.vision)[1]+1:size(creatures.vision)[1] + size(creatures.memory)[1], :)
+    nn_input_vision .= creatures.vision
+    nn_input_memory .= creatures.memory
+
+    nn_out = forward!(creatures.nn, creatures.input)
     nn_out_x = view(nn_out, 1, :)
     nn_out_y = view(nn_out, 2, :)
-    nn_out_xy = view(nn_out, 1:2, :)
     nn_out_mag = view(nn_out, 3, :)
     vel_x = view(creatures.velocity, 1, :)
     vel_y = view(creatures.velocity, 2, :)
+    nn_out_mem_val = view(nn_out, 3:size(creatures.memory)[1]+2, :)
+    nn_out_mem_prop = view(nn_out, size(creatures.memory)[1]+3:2+size(creatures.memory)[1]*2, :)
+    creatures.memory .= (nn_out_mem_val .* (nn_out_mem_prop .* .5 .+ .5)) .+ creatures.memory .* (nn_out_mem_prop .* -.5 .+ .5)
     creatures.scalars_temp .= nn_out_mag ./ sqrt.(nn_out_x .^ 2 + nn_out_y .^ 2)
     # creatures.velocity .+= nn_out_xy .* creatures.scalars_temp
     vel_x .+= nn_out_x .* creatures.scalars_temp
@@ -53,8 +71,19 @@ function update_creatures!(creatures::Creature)
     nothing
 end
 
+
+
 function render_creatures(creatures::Creature)
-    x = view(creatures.position, 1, :)
-    y = view(creatures.position, 2, :)
-    Raylib.DrawCircle.(Int.(round.(x)), Int.(round.(y)), 5.0, Raylib.RED)
+    pos = creatures.position
+
+    if typeof(pos) <: CuArray
+        pos = Matrix(pos)
+    end
+    x = view(pos, 1, :)
+    y = view(pos, 2, :)
+    c = Color(0, 255, 255, 255)
+    function draw_circ(x, y)
+        DrawCircle(x, y, 5.0, c)
+    end
+    draw_circ.(Int.(round.(x)), Int.(round.(y)))
 end
